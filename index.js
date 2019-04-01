eda7768afa9916cf599696e41d5f3a27eddf64a6
Igor Bragaia <igor.bragaia@gmail.com>
2019-04-01 01:47:38 -0300

    adding queries endpoint and heroku deploy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./queries')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -6,4 +7,9 @@ express()
   .get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
+  .get('/users', db.getUsers)
+  .get('/users/:id', db.getUserById)
+  .post('/users', db.createUser)
+  .put('/users/:id', db.updateUser)
+  .delete('/users/:id', db.deleteUser)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
