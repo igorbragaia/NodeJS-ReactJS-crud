e1b9bbada555084edaf19980135bff159e695b03
Igor Bragaia <igor.bragaia@gmail.com>
2019-04-02 17:49:50 -0300

    post request to subscribe user added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Container, Menu, Table } from 'semantic-ui-react';
+import { Container, Menu, Table, Form, Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   state = {
       activeItem: "users",
-      users: []
+      users: [],
+      name: "Igor Bragaia",
+      email: "igor.bragaia@gmail.com",
   }
 
   handleItemClick = (e, {name}) => {
@@ -19,10 +27,38 @@ class App extends Component {
     this.setState({activeItem: name});
   }
 
+  handleSubscribeClick = (event) => {
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email
+      })
+    };
+
+    fetch('/users', options)
+      .then(res => res.text())
+      .then(res => console.log(res))
+      .catch(() => {
+        console.log('failed to fetch users');
+      });
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value });
+  }
+
   fetchUsers = () => {
     fetch('/users')
       .then(res => res.json())
-      .then(users => this.setState({ users }));
+      .then(users => this.setState({ users }))
+      .catch(() => {
+        console.log('failed to fetch users');
+      });
   }
 
   componentDidMount() {
@@ -43,7 +79,17 @@ class App extends Component {
       case "subscribe":
         content =
           <div>
-            hello world 1
+            <Form>
+              <Form.Field>
+                <label>Name</label>
+                <input name='name' onChange={this.handleChange} placeholder='Igor Bragaia' />
+              </Form.Field>
+              <Form.Field>
+                <label>Email</label>
+                <input name='email' onChange={this.handleChange} placeholder='igor.bragaia@gmail.com' />
+              </Form.Field>
+              <Button onClick={this.handleSubscribeClick}>Submit</Button>
+            </Form>
           </div>;
         break;
       case "users":
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const db = require('./queries')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.json())
+  .use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
   .use(express.static(path.join(__dirname, 'client/build')))
   .get('/users', db.getUsers)
   .get('/users/:id', db.getUserById)
